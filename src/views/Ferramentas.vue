ec5613584adc05693e44b19212f7422558a35dec
igorjb360 <igorjb360@gmail.com>
2025-09-11 06:00:52 -0300

    Atualizando portifolio

--- src/views/Ferramentas.vue
+++ src/views/Ferramentas.vue
@@ -1,33 +1,53 @@
 <template>
-<div>
-  <h1>Ferramentas</h1>
-  <div>
-    <img src="@/assets/javascript-icon.svg" alt="Icone do Angular" />
-    <img src="@/assets/react-icon.svg" alt="Icone do React" />
-    <img src="@/assets/vuejs-icon.svg" alt="Icone do Vue.js" />    
-    <img src="@/assets/mysql-icon.svg" alt="Icone do Mysql" />
-    <img src="@/assets/typescript-icon.svg" alt="Icone do Typescript" />
-    <img src="@/assets/angularjs-icon.svg" alt="Icone do Angular.js" />
-    <img src="@/assets/html-icon.svg" alt="Icone do HTML" />
-    <img src="@/assets/css-icon.svg" alt="Icone do CSS" />
-    
+  <div class="ferramentas">
+    <h1>Ferramentas</h1>
+    <div class="icones">
+      <img src="@/assets/javascript-icon.svg" alt="Ícone do JavaScript" />
+      <img src="@/assets/react-icon.svg" alt="Ícone do React" />
+      <img src="@/assets/vuejs-icon.svg" alt="Ícone do Vue.js" />
+      <img src="@/assets/mysql-icon.svg" alt="Ícone do MySQL" />
+      <img src="@/assets/typescript-icon.svg" alt="Ícone do TypeScript" />
+      <img src="@/assets/angularjs-icon.svg" alt="Ícone do AngularJS" />
+      <img src="@/assets/html-icon.svg" alt="Ícone do HTML" />
+      <img src="@/assets/css-icon.svg" alt="Ícone do CSS" />
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style scoped>
-img {
-  width: 150px;
-  transition: transform 0.3s ease-in-out;
+.ferramentas {
+  text-align: center;
+  margin-top: 40px;
 }
 
-img:hover {
-  transform: scale(1.1);
+.ferramentas h1 {
+  font-size: 2rem;
+  margin-bottom: 20px;
+  color: #fff; /* ajusta pra combinar com fundo */
+}
+
+.icones {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+  gap: 20px;
+  justify-items: center;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+.icones img {
+  width: 100px;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  cursor: pointer;
+}
+
+.icones img:hover {
+  transform: scale(1.15);
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.3);
 }
 </style>
